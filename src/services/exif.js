827aa1a7fc00aa18612d3f41dde32b6cf2db946d
ronnanakibu <ronysihombing07@gmail.com>
2026-06-04 09:12:34 +0700

    deploy: sync auto 2026-06-04 02:12:34

--- src/services/exif.js
+++ src/services/exif.js
@@ -1,7 +1,7 @@
 import crypto from 'crypto'
 import webpmux from 'node-webpmux'
 
-export async function addExif(webpBuffer, packname = 'RonnBot', author = 'WABOT 2.0') {
+export async function addExif(webpBuffer, packname = 'ronnBot by ronnanakibu', author = 'https://github.com/ronnanakibu') {
     const json = {
         'sticker-pack-id': crypto.randomBytes(32).toString('hex'),
         'sticker-pack-name': packname,
@@ -12,7 +12,7 @@ export async function addExif(webpBuffer, packname = 'RonnBot', author = 'WABOT 
     let length = JSON.stringify(json).length
     const f = Buffer.from([0x49, 0x49, 0x2A, 0x00, 0x08, 0x00, 0x00, 0x00, 0x01, 0x00, 0x41, 0x57, 0x07, 0x00])
     const code = [0x00, 0x00, 0x16, 0x00, 0x00, 0x00]
-    
+
     if (length > 256) {
         length = length - 256
         code.unshift(0x01)
@@ -22,16 +22,16 @@ export async function addExif(webpBuffer, packname = 'RonnBot', author = 'WABOT 
 
     const fff = Buffer.from(code)
     const ffff = Buffer.from(JSON.stringify(json))
-    
+
     let lengthHex = length.toString(16)
     if (lengthHex.length < 2) lengthHex = '0' + lengthHex
     const ff = Buffer.from(lengthHex, 'hex')
-    
+
     const exifBytes = Buffer.concat([f, ff, fff, ffff])
-    
+
     const img = new webpmux.Image()
     await img.load(webpBuffer)
-    
+
     img.exif = exifBytes
     return await img.save(null) // Return as buffer
 }
